0170735a9e9b74ee63b789d275605b796507f272
Inscode <inshafahmedh@gmail.com>
2023-12-11 22:25:44 +0530

    did js work on pharmacist_prescription.html

---
+++ app/views/pharmacist/javascripts/pharmcist_prescription.js
@@ -0,0 +1,10 @@
+function openPopup() {
+    document.getElementById('dim-overlay').style.display = 'block';
+    document.getElementById('popup').style.display = 'block';
+}
+  
+function closePopup() {
+document.getElementById('dim-overlay').style.display = 'none';
+document.getElementById('popup').style.display = 'none';
+}
+
