25603259cab44cd436e7b340ee5889e253fd4aa2
SenuriWickramasinghe <mashawickramasinghe04@gmail.com>
2023-12-30 14:51:26 +0530

    Appointment view

--- app/models/M_Patient.php
+++ app/models/M_Patient.php
@@ -1,14 +1,25 @@
 <?php
-class M_Patient {
+class M_Patient
+{
     private $db;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->db = new Database();
     }
 
-    public function getAppointments(){
+    public function getAppointments()
+    {
         $this->db->query('SELECT * FROM appointments WHERE patient_ID = 125');
         $result = $this->db->resultSet();
+        return $result;
+    }
+
+    public function viewAppointment($appointment_ID)
+    {
+        $this->db->query('SELECT * FROM appointments WHERE patient_ID = 125 AND appointment_ID = :appointment_id');
+        $this->db->bind(':appointment_id', $appointment_ID);
+        $result = $this->db->single(); // Assuming you have a method to retrieve a single row
         return $result;
     }
 }
