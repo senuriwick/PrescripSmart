45ec201d12a021a61ff4f8789cd080d5f3a31250
Ayasmanthi <ayasmanthi@gmail.com>
2024-01-09 09:48:37 +0530

    set URLROOT for css files in doctor

--- app/views/doctor/add_prescription.php
+++ app/views/doctor/add_prescription.php
@@ -10,16 +10,16 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A300%2C400%2C500%2C600" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter%3A300%2C400%2C500%2C600" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
-    <link rel="stylesheet" href="../../public/css/doctor/add_prescription.css" />
-    <link rel="stylesheet" href="../../public/css/doctor/sideMenu&navBar.css" />
-    <script src="../../public/js/doctor/main.js"></script>
+    <link rel="stylesheet" href="<?php echo URLROOT;?>/public/css/doctor/add_prescription.css" />
+    <link rel="stylesheet" href="<?php echo URLROOT;?>/public/css/doctor/sideMenu&navBar.css" />
+    <script src="<?php echo URLROOT;?>/public/js/doctor/main.js"></script>
 </head>
 
 <body>
     <div class="content">
         <div class="sideMenu">
             <div class="logoDiv">
-                <img class="logoImg" src="../../public/img/doctor/Untitled design (5) copy 2.png" />
+                <img class="logoImg" src="<?php echo URLROOT;?>/public/img/doctor/Untitled design (5) copy 2.png" />
             </div>
 
             <!-- <div class="userDiv">
@@ -52,14 +52,14 @@
         <div class="container">
             <div class="navBar">
                 <div class="navBar">
-                    <img src="../../public/img/doctor/user.png" alt="user-icon">
+                    <img src="<?php echo URLROOT;?>/public/img/doctor/user.png" alt="user-icon">
                     <p>USERNAME</p>
                 </div>
             </div>
             <div class="main">
                 <div class="main-Container">
                     <div class="userInfo">
-                        <img src="../../public/img/doctor/profile.png" alt="profile-pic">
+                        <img src="<?php echo URLROOT;?>/public/img/doctor/profile.png" alt="profile-pic">
                         <div class="userNameDiv">
                             <p class="name"><?php echo $data['patient']->patient_name;?></p>
                             <p class="role">Patient</p>
