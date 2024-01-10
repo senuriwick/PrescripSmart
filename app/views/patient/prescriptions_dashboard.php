d62312d85f67ce6afabc592ed8839bb28fc0fa6c
SenuriWickramasinghe <mashawickramasinghe04@gmail.com>
2024-01-10 20:29:20 +0530

    Prescription dashboard v1.1

--- app/views/patient/prescriptions_dashboard.php
+++ app/views/patient/prescriptions_dashboard.php
@@ -98,18 +98,25 @@
 
               <div class="modal-content">
                 <span class="close">&times;</span>
-                <a href="www.prescripsamert.com">www.prescripsmart.com</a>
+                <a href="www.prescripsmart.com">www.prescripsmart.com</a>
                 <div class="model-head">
                   <img src="<?php echo URLROOT; ?>/public/img/doctor/qr.png" alt="qr-img" />
                   <h4><u>CONFIDENTIAL PRESCRIPTION</u></h4>
                   <i class="fa-solid fa-circle-arrow-up"></i>
                 </div>
                 <div class="model-details">
-                  <div>Prescription ID: #<?php echo $prescription->prescription_ID; ?></div>
+                  <div>Prescription ID: #
+                    <?php echo $prescription->prescription_ID; ?>
+                  </div>
                   <div>Patient: S.Perera</div>
-                  <div>Pres Date & Time: <?php echo $prescription->prescription_Date; ?> 10:00 AM</div>
+                  <div>Pres Date & Time:
+                    <?php echo $prescription->prescription_Date; ?> 10:00 AM
+                  </div>
                   <div>Age: 22 Yrs</div>
-                  <div>Referred by: Dr.<?php echo $prescription->fName; ?> <?php echo $prescription->lName; ?></div>
+                  <div>Referred by: Dr.
+                    <?php echo $prescription->fName; ?>
+                    <?php echo $prescription->lName; ?>
+                  </div>
                 </div>
                 <div class="pres-box">
                   <label>Diagnosis</label>
@@ -150,11 +157,11 @@
                   </table>
                 </div>
                 <div class="notice">(For viewing purpose only)</div>
+                <button type="button" id="qr" class="qr-button">View QR</button>
               </div>
             </div>
-
-          </div>
-        <?php endforeach; ?>
+          <?php endforeach; ?>
+        </div>
 
         <p class="addnewHeading">Add new</p>
         <div class="addnew">
