4e686ae8b3582d8892106fce4340b4e787a37f88
SenuriWickramasinghe <mashawickramasinghe04@gmail.com>
2024-01-15 19:57:14 +0530

    QR code test file

---
+++ simple.php
@@ -0,0 +1,21 @@
+<?php
+// (A) LOAD QR CODE LIBRARY
+require "vendor/autoload.php";
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Writer\PngWriter;
+
+// (B) CREATE QR CODE
+$qr = QrCode::create("https://code-boxx.com");
+$writer = new PngWriter();
+$result = $writer->write($qr);
+
+// (C) OUTPUT QR CODE
+// (C1) SAVE TO FILE
+$result->saveToFile(__DIR__ . "/qr.png");
+
+// (C2) DIRECT OUTPUT
+header("Content-Type: " . $result->getMimeType());
+echo $result->getString();
+
+// (C3) GENERATE DATA URI
+// echo "<img src='{$result->getDataUri()}'>";
