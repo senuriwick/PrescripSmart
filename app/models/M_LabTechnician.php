8ba2cde85f4568e6d62f5ecc41826fa58c06f127
Ayasmanthi <ayasmanthi@gmail.com>
2024-04-22 11:13:05 +0530

    report update delete marked part of labtechnician

--- app/models/M_LabTechnician.php
+++ app/models/M_LabTechnician.php
@@ -8,64 +8,65 @@ class M_LabTechnician {
     }
 
     public function repotsToUploadList(){
-        $this->db->query('SELECT reports.* , patientdetails.* FROM `reports` LEFT JOIN `patientdetails` 
-        ON reports.patient_id=patientdetails.patient_id WHERE file_name="" GROUP BY patientdetails.patient_id');
+        $this->db->query('SELECT lab_reports.* , patients.* FROM `lab_reports` LEFT JOIN `patients` 
+        ON lab_reports.patient_ID=patients.patient_ID WHERE lab_reports.report="" OR lab_reports.date_of_conduct="" GROUP BY patients.patient_ID');
         $results = $this->db->resultSet();
         return $results;
     }
 
     public function getPatient($patientID){
-        $this->db->query('SELECT * FROM patientdetails WHERE patient_id=:id');
+        $this->db->query('SELECT * FROM patients WHERE patient_ID=:id');
         $this->db->bind(':id',$patientID);
         $results = $this->db->single();
         return $results;
     }
 
     public function getTests($patientID){
-        $this->db->query('SELECT test.* , doctordetails.* FROM `test` LEFT JOIN `doctordetails` 
-        ON test.doctor_id=doctordetails.doctor_id WHERE test.patient_id=:id AND test.marked=""');
+        $this->db->query('SELECT lab_reports.* , doctors.*, tests.* FROM `lab_reports` LEFT JOIN `doctors` 
+        ON lab_reports.doctor_ID=doctors.doctor_ID LEFT JOIN `tests`ON tests.test_ID=lab_reports.test_ID WHERE lab_reports.patient_ID=:id AND date_of_conduct IS NULL');
         $this->db->bind(':id',$patientID);
         $results = $this->db->resultSet();
         return $results;
     }
 
     public function getTestCount($patientID){
-        $this->db->query('SELECT * FROM test WHERE patient_id=:id AND marked=""');
+        $this->db->query('SELECT * FROM lab_reports WHERE patient_ID=:id AND date_of_conduct IS NULL');
         $this->db->bind(':id',$patientID);
         $results=$this->db->resultSet();
         return $this->db->rowCount();
     }
 
     public function markedTest($testNo){
-        $this->db->query('UPDATE test SET marked="1" WHERE test_no=:num');
+        $this->db->query('UPDATE lab_reports SET date_of_conduct=NOW() WHERE report_ID=:num');
         $this->db->bind(':num',$testNo);
         $this->db->execute();
     }
 
-    public function getReportid($testno){
-        $this->db->query('SELECT report_id FROM test WHERE test_no=:testid');
-        $this->db->bind(':testid',$testno);
-        $results = $this->db->single();
-        return $results;
-    }
+    // public function getReportid($testno){
+    //     $this->db->query('SELECT report_ID FROM lab_reports WHERE report_ID=:testid');
+    //     $this->db->bind(':testid',$testno);
+    //     $results = $this->db->single();
+    //     return $results;
+    // }
 
-    public function uploadReport($testid,$reportname,$patientid){
-        $this->db->query('INSERT INTO reports (test_id,patient_id,file_name) VALUES (:testid,:patientid,:reportname)');
+    public function uploadReport($testid,$reportname,$filesize){
+        $this->db->query('UPDATE lab_reports SET report=:reportname, size=:filesize, date_of_report=NOW() WHERE report_ID=:testid');
         $this->db->bind(':testid',$testid);
-        $this->db->bind(':patientid',$patientid);
+        $this->db->bind(':filesize',$filesize);
         $this->db->bind(':reportname',$reportname);
         $this->db->execute();
     }
 
     public function deleteReport($reportid){
-        $this->db->query('DELETE FROM reports WHERE report_id=:reportid');
+        $this->db->query('UPDATE lab_reports SET report="",date_of_report=NULL WHERE report_ID=:reportid');
         $this->db->bind(':reportid',$reportid);
         $this->db->execute();
     }
 
-    public function removeReport($testid){
-        $this->db->query('UPDATE test SET report_id=0 WHERE test_no=:testid');
+    public function checkReport($testid){
+        $this->db->query('SELECT * FROM lab_reports WHERE report_ID=:testid');
         $this->db->bind(':testid',$testid);
-        $this->db->execute();
+        $results = $this->db->single();
+        return $results;
     }
 }
