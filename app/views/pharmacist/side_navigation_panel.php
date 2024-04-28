b197267153053b5999d3bf24280534fc20fdd787
Inscode <inshafahmedh@gmail.com>
2024-04-28 06:11:32 +0530

    pharmacist additional work

--- app/views/pharmacist/side_navigation_panel.php
+++ app/views/pharmacist/side_navigation_panel.php
@@ -32,7 +32,7 @@
             </div>
 
             <div class="menu-item analysis">
-                <img src="<?php echo URLROOT ?>/public/img/nurse/account_icon.png" alt="icon">
+                <img src="<?php echo URLROOT ?>/public/img/pharmacist/Charts.png" alt="icon">
                 <a href="<?php echo URLROOT; ?>/pharmacist/analysis" id="profile">Medication Analysis</a>
             </div>
 
